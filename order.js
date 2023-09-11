aab3fee716714748785707f0340ef882dee32722
Khan Vichet <khanvichet1@gamil.com>
2023-09-12 03:11:59 +0700

    second commit

---
+++ order.js
@@ -0,0 +1,27 @@
+const Orders=[
+    {
+        productName :'Foldable Mini Drone',
+        productNumber :'73247',
+        paymentState :'Due',
+        shipping : 'Pending'
+    },
+    {
+        productName :'Foldable Mini Drone',
+        productNumber :'73247',
+        paymentState :'Due',
+        shipping : 'Delined'
+    },
+    {
+        productName :'Foldable Mini Drone',
+        productNumber :'73247',
+        paymentState :'Due',
+        shipping : 'Delivered'
+    },
+    {
+        productName :'Foldable Mini Drone',
+        productNumber :'73247',
+        paymentState :'Due',
+        shipping : 'Pending'
+    }
+
+]
